e69faf979673f0a87bb9e79dd4f60e5cbd1d9d4c
sanzhh <sanzhar.maratov@proton.me>
2023-09-24 12:27:18 +0600

    [added] read json from request to httpkit/json

--- httpkit/json/json.go
+++ httpkit/json/json.go
@@ -1,7 +1,9 @@
 package json
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/goccy/go-json"
@@ -33,6 +35,11 @@ func BadRequest(w http.ResponseWriter, data any) error {
 	return Write(w, http.StatusBadRequest, data)
 }
 
+func InternalServerError(w http.ResponseWriter) {
+	w.WriteHeader(500)
+	_, _ = w.Write([]byte(NewError("internal server error").Error()))
+}
+
 func Write(w http.ResponseWriter, status int, data any) error {
 	w.Header().Set("Content-Type", "application/json")
 	json, err := json.Marshal(data)
@@ -47,7 +54,37 @@ func Write(w http.ResponseWriter, status int, data any) error {
 	return nil
 }
 
-func InternalServerError(w http.ResponseWriter) {
-	w.WriteHeader(500)
-	_, _ = w.Write([]byte(NewError("internal server error").Error()))
+func Read(r *http.Request, dst any) error {
+	if err := json.NewDecoder(r.Body).Decode(dst); err != nil {
+		var (
+			syntaxErr           *json.SyntaxError
+			unmarshalTypeErr    *json.UnmarshalTypeError
+			invalidUnmarshalErr *json.InvalidUnmarshalError
+		)
+
+		switch {
+		case errors.As(err, &syntaxErr):
+			return fmt.Errorf("badly formed JSON (at character %d)", syntaxErr.Offset)
+
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("request closed")
+
+		case errors.As(err, &unmarshalTypeErr):
+			if unmarshalTypeErr.Field != "" {
+				return fmt.Errorf("incorrect type (at field %s)", unmarshalTypeErr.Field)
+			}
+
+			return fmt.Errorf("incorrect type (at character %d)", unmarshalTypeErr.Offset)
+
+		case errors.Is(err, io.EOF):
+			return errors.New("empty JSON")
+
+		case errors.As(err, &invalidUnmarshalErr):
+			panic("destination must be non nil pointer")
+		default:
+			return err
+		}
+	}
+
+	return nil
 }
